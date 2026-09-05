938437facee76ca2c1a40845d565755f4f566e87
kyawnyein8-sudo <kyaw.nyein8@gmail.com>
2026-09-05 19:36:19 +0630

    Update service worker to version 2 and add icons

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,40 @@
-const CACHE_NAME = "myanmar-construction-calc-v1";
+const CACHE_NAME = "myanmar-construction-calc-v2";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install Event - Caching Assets
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
+// Activate Event - Clearing Old Caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
+// Fetch Event - Serve from Cache or Network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
